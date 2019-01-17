f2ece30602e2d5310e5c062a1111d504d1bb39c4
karanbalaji <karanarjunb@gmail.com>
2019-01-17 15:02:22 -0500

    Updates

---
+++ precache-manifest.c3be4f5e214648a51ea63a84ab5917f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/college-app/static/media/user.7a21cbe9.svg"
+  },
+  {
+    "revision": "34d8fd6717af160d7b8e46bfd6dfc681",
+    "url": "/college-app/static/media/karan.34d8fd67.jpeg"
+  },
+  {
+    "revision": "9df05122b8d49373eae4",
+    "url": "/college-app/static/js/runtime~main.9df05122.js"
+  },
+  {
+    "revision": "ae9543936605a22d871e",
+    "url": "/college-app/static/js/main.ae954393.chunk.js"
+  },
+  {
+    "revision": "f7783b4f7fd7431455eb",
+    "url": "/college-app/static/js/1.f7783b4f.chunk.js"
+  },
+  {
+    "revision": "f1f03ed45f2a48a9a02a825aa27a219a",
+    "url": "/college-app/index.html"
+  }
+];
